db7a705803d3dc072743013d63d7e31ef928fe2a
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-01-13 15:56:23 +0100

    Add countries dataset

---
+++ R/wdpa_countries.R
@@ -0,0 +1,18 @@
+#' @title List of countries distributed in the UNEP regions
+#' @description This dataset contains informations about countries listed on the Protected Planet project (\link{https://www.protectedplanet.net})
+#'
+#' @format A data frame with 248 rows (countries) and 5 columns:
+#' \describe{
+#'   \item{region_name}{The name of the region;}
+#'   \item{region_iso2}{The ISO-2 code of the region;}
+#'   \item{country_name}{The name of the country;}
+#'   \item{country_iso3}{The ISO-3 code of the country;}
+#'   \item{pas_count}{Number of protected areas per country.}
+#' }
+#'
+#' This dataset has been built using the Protected Planet API the 2020/01/13. For a more up-to-date version, please use the function \code{wdpa::get_countries(update = TRUE)}.
+#'
+#' @source \link{https://www.protectedplanet.net/c/unep-regions}
+
+
+"wdpa_countries"
