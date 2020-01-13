7a9d7333ce22b19f4675cc97e63567ad2effa97a
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-01-13 18:07:49 +0100

    Update Internal function cross-references

--- R/get_token.R
+++ R/get_token.R
@@ -1,15 +1,15 @@
 #' @title Check Protected Planet API token
 #'
-#' @description This function checks if the user has stored a valid Protected Planet API token in the R environment (.Renviron) file under the key 'WDPA_KEY'.
+#' @description This function checks if the user has stored a valid Protected Planet API token in the R environment (.Renviron) file under the key \code{WDPA_KEY}.
 #'
-#' @param key The name of the token value stored in the .Renviron file (recommended key name: 'WDPA_KEY').
+#' @param key The name of the token value stored in the .Renviron file (recommended key name: \code{WDPA_KEY}).
 #'
 #' @return A vector of length one with the value of the API token.
 #'
 #' @details Before using this package for the first time, the user must follow these steps:
 #' \itemize{
 #'   \item Fill in the form available at: \url{https://api.protectedplanet.net/request} to obtain a personal API token;
-#'   \item Store the token in the .Renviron file under the key 'WDPA'. User can use the function \code{usethis::edit_r_environ()}.
+#'   \item Store the token in the .Renviron file under the key \code{WDPA_KEY}. User can use the function \code{usethis::edit_r_environ()}.
 #' }
 #'
 #' @importFrom httr GET
