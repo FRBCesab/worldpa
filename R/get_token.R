99afa526bb8047185da23ddaec0045b7540ce4c5
Nicolas Casajus <nicolas.casajus@gmail.com>
2021-02-12 14:40:56 +0100

    :boom: Handle Error 500

--- R/get_token.R
+++ R/get_token.R
@@ -3,8 +3,8 @@
 #' This function checks if the user has stored a valid Protected Planet API
 #'   token in the R environment file (`.Renviron`) under the key `WDPA_KEY`.
 #'
-#' @param key a character providing the token value stored in the `.Renviron`
-#'   file (recommended key name: `WDPA_KEY`).
+#' @param key a character providing the WDPA token value stored in the
+#'   `.Renviron` file (recommended key name: `WDPA_KEY`).
 #'
 #' @return A vector of length one with the value of the API token.
 #'
@@ -37,10 +37,11 @@ get_token <- function(key = "WDPA_KEY") {
   if (wdpa_key == "") {
     stop("Missing WDPA API Token.\n",
          "Please ensure:\n",
-         " 1. You completed this form ",
+         " 1. to complete this form ",
          "<https://api.protectedplanet.net/request> ",
          "to get your own token, and\n",
-         " 2. You stored the value in the .Renviron with the name WDPA_KEY.")
+         " 2. to store the value in the `.Renviron` file with the name ",
+         "WDPA_KEY.")
   }
 
   response <- httr::GET(paste0("https://api.protectedplanet.net/test?token=",
@@ -49,10 +50,15 @@ get_token <- function(key = "WDPA_KEY") {
   if (response$"status" == 401) {
     stop("Invalid WDPA API Token.\n",
          "Please ensure:\n",
-         " 1. You completed this form ",
+         " 1. to complete this form ",
          "<https://api.protectedplanet.net/request> ",
          "to get your own token, and\n",
-         " 2. You stored the value in the .Renviron with the name WDPA_KEY.")
+         " 2. to store the value in the `.Renviron` file with the name ",
+         "WDPA_KEY.")
+  }
+
+  if (response$"status" == 500) {
+    stop("Internal Server Error.")
   }
 
   if (response$"status" != 200) {
