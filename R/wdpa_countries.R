dcfb92ed3bd9a60ea9a3eca4857f954adc974490
Nicolas Casajus <nicolas.casajus@gmail.com>
2021-02-12 09:05:42 +0100

    :sparkles: Update World Countries Dataset

--- R/wdpa_countries.R
+++ R/wdpa_countries.R
@@ -1,18 +1,19 @@
-#' @title List of countries distributed in the UNEP regions
-#' @description This dataset contains informations about countries listed on the Protected Planet project (\url{https://www.protectedplanet.net})
+#' World Countries Distributed in the UNEP-WCMC Regions
 #'
-#' @format A data frame with 248 rows (countries) and 5 columns:
+#' This dataset contains informations about countries listed on the Protected
+#'   Planet project (\url{https://www.protectedplanet.net}). This dataset has
+#'   been built using the Protected Planet API the 2021/02/12. For a more
+#'   up-to-date version, please use the function `get_countries`.
+#'
+#' @format A data frame with 248 rows (countries) and the following 5 columns:
 #' \describe{
-#'   \item{region_name}{The name of the region;}
-#'   \item{region_iso2}{The ISO-2 code of the region;}
-#'   \item{country_name}{The name of the country;}
-#'   \item{country_iso3}{The ISO-3 code of the country;}
-#'   \item{pas_count}{Number of protected areas per country.}
+#'   \item{region_name}{the name of the region}
+#'   \item{region_iso2}{the ISO-2 code of the region}
+#'   \item{country_name}{the name of the country}
+#'   \item{country_iso3}{the ISO-3 code of the country}
+#'   \item{pas_count}{the number of protected areas per country}
 #' }
 #'
-#' This dataset has been built using the Protected Planet API the 2020/01/13. For a more up-to-date version, please use the function \code{get_countries()}.
-#'
 #' @source \url{https://www.protectedplanet.net/c/unep-regions}
-
 
 "wdpa_countries"
